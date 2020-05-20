0bbc649dbd1ed852926ecf24cf9b56fc85ed8525
priyanshu <priyanshubashu7421@gmail.com>
2020-05-20 14:08:35 +0530

    Completed Backend of Splash Screen

---
+++ app/src/main/java/com/sonunayan48/android/covidtracker/SplashScreenActivity.java
@@ -0,0 +1,36 @@
+package com.sonunayan48.android.covidtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_splash_screen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+
+        /** Duration of wait **/
+        int SPLASH_DISPLAY_LENGTH = 2000;
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent startStateActivity = new Intent(SplashScreenActivity.this, StateActivity.class);
+                SplashScreenActivity.this.startActivity(startStateActivity);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
+
+
+
+
