aa3b553d110e890822fac953f167b37a37470368
MAYANK KUMAR <sonunayan48@gmail.com>
2020-05-24 23:55:24 +0530

    Added Animation on Splash Screen

--- app/src/main/java/com/sonunayan48/android/covidtracker/SplashScreenActivity.java
+++ app/src/main/java/com/sonunayan48/android/covidtracker/SplashScreenActivity.java
@@ -5,15 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private ImageView appIconImg;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_splash_screen);
-
+        RotateAnimation rotate = new RotateAnimation(0, 359, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(3000);
+        rotate.setInterpolator(new LinearInterpolator());
+        appIconImg = findViewById(R.id.covid19);
+        appIconImg.startAnimation(rotate);
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
 
